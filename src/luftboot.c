26456fe46227fd8059ea6ffb32db6f5463238c31
Felix Ruess <felix.ruess@gmail.com>
2014-02-01 19:48:59 +0100

    fix systick clock source
    after compatibility with old clocksource was removed in libopencm3 in bfc65271c083ad2cd79e61901a769d8c354d863b

--- src/luftboot.c
+++ src/luftboot.c
@@ -483,7 +483,7 @@ int main(void)
 
 	gpio_init();
 
-	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
 	systick_set_reload(900000);
 	systick_interrupt_enable();
 	systick_counter_enable();
